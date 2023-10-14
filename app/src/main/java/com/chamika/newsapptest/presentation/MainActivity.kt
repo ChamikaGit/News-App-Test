fba26a2e425b975ee15e09408e7c35e5c68f1211
Chamika Weerasekara <>
2023-10-14 12:36:04 +0530

    project structure added

--- app/src/main/java/com/chamika/newsapptest/presentation/MainActivity.kt
+++ app/src/main/java/com/chamika/newsapptest/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.chamika.newsapptest
+package com.chamika.newsapptest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chamika.newsapptest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
